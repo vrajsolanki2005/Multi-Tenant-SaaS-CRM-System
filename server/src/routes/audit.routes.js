b74a23ab00de91a57c72352205188ffc93c73d26
vrajsolanki2005 <144586038+vrajsolanki2005@users.noreply.github.com>
2026-03-04 21:25:58 +0530

    Update audit.routes.js

--- server/src/routes/audit.routes.js
+++ server/src/routes/audit.routes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const auditController = require('../controllers/auditController');
-const { verifyToken } = require('../middlewares/authMiddleware');
-const { authorize } = require('../middlewares/rbacMiddleware');
+const { authMiddleware } = require('../middlewares/authMiddleware');
+const { allowRoles } = require('../middlewares/rbacMiddleware');
 
-router.get('/', verifyToken, authorize(['SuperAdmin', 'Admin']), auditController.getAuditLogs);
+router.get('/', authMiddleware, allowRoles('SuperAdmin', 'Admin'), auditController.getAuditLogs);
 
 module.exports = router;
